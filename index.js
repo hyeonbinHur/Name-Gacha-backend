ea92d6a24d4103c48c07fa64ac86f1ae6124ddff
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-05-30 20:33:46 +0900

    connect with pg and create simple api

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const { Client } = require('pg');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+const app = express();
+const port = 8000;
+
+// PostgreSQL connection setup
+const dbClient = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'Gjgusqls1!',
+    database: 'NameGachaDatabase',
 });
 
-app.listen(3000);
+// Connect to the database
+dbClient.connect((err) => {
+    if (err) {
+        console.error('Failed to connect to the database', err.stack);
+    } else {
+        console.log('Database connected');
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Endpoint to get current time (example you had)
+app.get('/time', (req, res) => {
+    dbClient.query('SELECT NOW()', (err, queryRes) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Database query failed');
+        } else {
+            res.send('Current time: ' + queryRes.rows[0].now);
+        }
+    });
+});
+
+// New endpoint to get data from the 'test' table
+app.get('/test-data', (req, res) => {
+    dbClient.query('SELECT * FROM public.test', (err, queryRes) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Failed to retrieve data');
+        } else {
+            res.json(queryRes.rows);
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
