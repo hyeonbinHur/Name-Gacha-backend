4adfb900cb48ace2f5e3f4a02436690a08bee901
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-05-31 00:11:05 +0900

    Delete project API

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -14,7 +14,6 @@ router.get('/projects', (req, res) => {
     });
 });
 
-// POST 요청: 새로운 프로젝트 추가
 router.post('/projects', (req, res) => {
     const { projectName } = req.body;
     const query =
@@ -27,6 +26,24 @@ router.post('/projects', (req, res) => {
             res.status(500).send('Failed to insert data');
         } else {
             res.status(201).json(queryRes.rows[0]); // 새로 추가된 프로젝트 반환
+        }
+    });
+});
+
+router.delete('/projects/:id', (req, res) => {
+    const projectId = req.params.id;
+    const query =
+        'DELETE FROM public.projects WHERE "projectId" = $1 RETURNING *';
+    const values = [projectId];
+
+    dbClient.query(query, values, (err, queryRes) => {
+        if (err) {
+            console.error(err.stack);
+            res.status(500).send('Failed to delete data');
+        } else if (queryRes.rowCount === 0) {
+            res.status(404).send('Project not found');
+        } else {
+            res.status(200).json(queryRes.rows[0]); // 삭제된 프로젝트 반환
         }
     });
 });
